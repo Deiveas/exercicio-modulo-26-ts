646535a92ad113ba6b441e9373c432a1e8b75fa2
Deive Silva <deiveas@gmail.com>
2024-04-02 10:28:32 -0300

    Exercício modulo 26 TypeScript entregue

---
+++ src/exercicio_modulo_26.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Testando as funções
+console.log(multiplicacao(5, 3)); // Saída esperada: 15
+console.log(saudacao("João")); // Saída esperada: Olá João
---
+++ src/exercicio_modulo_26.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Testando as funções
+console.log(multiplicacao(5, 3)); // Saída esperada: 15
+console.log(saudacao("João")); // Saída esperada: Olá João
